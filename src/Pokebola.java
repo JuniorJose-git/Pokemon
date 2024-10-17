7e863e543a319e134d14c94c5f6aab539fd2a7f5
Ysadora Sistini <ysadora.sistini@edu.unifil.br>
2024-10-17 20:11:34 -0300

    construtores pokebola

--- src/Pokebola.java
+++ src/Pokebola.java
@@ -1,3 +1,16 @@
-public class Pokebola {
+public abstract class Pokebola {
+
+     private String esferica;
+     private String metade_inferior = "branca";
+     private String botao_central;
+
+    public Pokebola(String esferica, String metade_inferior, String botao_central) {
+        this.esferica = esferica;
+        this.metade_inferior = metade_inferior;
+        this.botao_central = botao_central;
+    }
+
+
+
 
 }
