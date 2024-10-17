cb5d789918b3ed383f923c33db00426c6aa9fd93
jose <joseronaldosantus@gmail.com>
2024-10-17 20:17:47 -0300

    conflito

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,4 +1,10 @@
 public class Pokemon {
 
+    private String name;
+    private String[] tipo;
+    private int hp;
+    private int ataque;
+    private int defesa;
+    private int velocidade;
 
 }
